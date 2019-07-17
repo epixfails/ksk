ff1cea878e873fcb2c7b39fc87592997d0d748cd
Ivlev Michael <ivle.michael@gmail.com>
2019-07-17 23:19:02 +0300

    mark up header

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+
+$( document ).ready(function() {
+  
+  // БУРГЕР
+  $('.main-nav__toggle').click(function(e) {
+    e.preventDefault;
+    $('.main-nav__burger').toggleClass('main-nav__burger--active');
+    $('.main-nav__menu').toggleClass('main-nav__menu--active');
+  });
+
+  // ВЫПАДАЮЩЕЕ МЕНЮ
+  $('.main-nav__link').click(function(){
+    var width = $(window).width();
+
+    $(this).toggleClass('main-nav__link--active').next().slideToggle();
+    
+    if (width >= 1200) {
+      $('.main-nav__link').not(this).removeClass('main-nav__link--active').next().slideUp();
+    }
+  });
+
+  // СЛАЙДЕР
+  $('.promo__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+ 
+});
